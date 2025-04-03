2ed28782acf6955e2685b0bbd69b84e3b7c4ca1c
Thibaud Kloczko <thibaud.kloczko@inria.fr>
2025-04-03 15:21:33 +0200

    Add common and persitent_cohomology

--- src/cmake/modules/GUDHIExport.cmake
+++ src/cmake/modules/GUDHIExport.cmake
@@ -6,18 +6,20 @@ configure_package_config_file(
   ${CMAKE_SOURCE_DIR}/src/cmake/modules/GUDHIConfig.cmake.in
   ${CMAKE_BINARY_DIR}/GUDHIConfig.cmake
   INSTALL_DESTINATION
-  ${CMAKE_INSTALL_LIBDIR}/cmake/gudhi)
+  ${CMAKE_INSTALL_LIBDIR}/cmake/gudhi
+)
 
 write_basic_package_version_file(
   ${CMAKE_BINARY_DIR}/GUDHIConfigVersion.cmake
   VERSION       ${GUDHI_VERSION}
-  COMPATIBILITY AnyNewerVersion)
+  COMPATIBILITY AnyNewerVersion
+)
 
 install(
   FILES       ${CMAKE_BINARY_DIR}/GUDHIConfig.cmake
               ${CMAKE_BINARY_DIR}/GUDHIConfigVersion.cmake
-  DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/gudhi)
-endif()
+  DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/gudhi
+)
 
 ######################################################################
 ### GUDHIExport.cmake ends here
